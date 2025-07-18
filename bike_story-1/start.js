759c2e26fe0376b18a64041e4b52125b2716d5dd
New_Lord_8951 <lukyanovivan83@gmail.com>
2025-07-18 21:42:37 +0300

    Работать

--- bike_story-1/start.js
+++ bike_story-1/start.js
@@ -3,4 +3,12 @@ document.getElementById("myButton").addEventListener("click", () => {
   });
 
 
-  
+    document.querySelector('.j1').addEventListener('click', function() {
+    const message = document.getElementById('message');
+    message.style.display = 'block';
+    
+    // Скрываем сообщение через 3 секунды
+    setTimeout(() => {
+      message.style.display = 'none';
+    }, 3000);
+  });
